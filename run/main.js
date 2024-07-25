493d41a4be6e0ef6126fadea075f676c82b2f398
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2024-07-25 18:36:34 +0000

    Finally found a fix for the input bug.

--- run/main.js
+++ run/main.js
@@ -15,14 +15,37 @@ const progIDs = new Set(["0", "1", "2", "3", "4", "5"]);
     for (let i = 0; i < progAscii.length; ++i) {
         progBytes[i] = progAscii.charCodeAt(i);
     }
-    let prog = new TextDecoder('utf-8').decode(progBytes);
+    let progContent = new TextDecoder('utf-8').decode(progBytes);
 
+    let prog = progContent;
     // Work around weird bug in Pyscript.
-    // Previously printed lines are duplicated by an input with an empty string.
-    prog = prog.replace(new RegExp("input\\(\\)", "g"), "input(\"> \")");
+    // Previously printed lines are duplicated by an input 
+    // with an empty string or contains newlines.
+    // let prog = "";
+    // let progLines = progContent.split("\n");
+    // progLines.forEach(line => {
+    //     if (line.includes("input(")) {
+    //         prog += line.replace(/\\n"\)/g, "\\n> \")");
+    //     } else {
+    //         prog += line;
+    //     }
+    //     prog += "\n";
+    // });
+    // prog = prog.replace(/input\(\)/g, "input(\"> \")");
+    // console.log(prog);
 
+    // cowsay module required for one group.
     document.body.innerHTML += `
-<script id="python-script" type="py" terminal worker>
+<script id="python-script" type="py" config='{"packages":["cowsay"]}' terminal worker>
+# Work around weird bug in Pyscript.
+# Previously printed lines are duplicated by an input 
+# with an empty string or contains newlines.
+_original_input_ = input
+def input(prompt: str = ""):
+    print(prompt, end="")
+    if not prompt or prompt.endswith("\\n"):
+        return _original_input_("> ")
+    return _original_input_()
 ${prog}
 print("Program exited successfully.")
 </script>
